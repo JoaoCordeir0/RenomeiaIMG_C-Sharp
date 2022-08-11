5cca611307fce25cb18721004e43478172b1b4ea
Joao.Cordeiro <joaocordeiro2134@gmail.com>
2022-08-11 11:57:35 -0300

    Add projeto

---
+++ RenameImg/RenameImg/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenameImg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            var files = Directory.GetFiles(@"D:\Caminho");
+
+            foreach (var file in files)
+            {
+                var name = new FileInfo(file).FullName;
+
+                File.Move(name, name.Replace("IMG_", "newname"));
+            }
+        }
+    }
+}
